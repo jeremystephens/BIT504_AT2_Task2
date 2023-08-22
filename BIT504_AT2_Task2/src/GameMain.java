49ef0818a70d9d36d0542a86ca5b6b39219d8cc7
jeremystephens <jermsz@gmail.com>
2023-08-22 15:00:51 +1200

    Initialised the cells array using ROWS and COLS constants

--- BIT504_AT2_Task2/src/GameMain.java
+++ BIT504_AT2_Task2/src/GameMain.java
@@ -17,7 +17,7 @@ public class GameMain extends JPanel implements MouseListener{
 	//drawing canvas
 	public static final int CANVAS_WIDTH = CELL_SIZE * COLS;
 	public static final int CANVAS_HEIGHT = CELL_SIZE * ROWS;
-	//Noughts and Crosses are displayed inside a cell, with padding from border
+	//Naughts and Crosses are displayed inside a cell, with padding from border
 	public static final int CELL_PADDING = CELL_SIZE / 6;    
 	public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING * 2;    
 	public static final int SYMBOL_STROKE_WIDTH = 8;
