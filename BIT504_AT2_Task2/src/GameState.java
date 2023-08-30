e713b70a53f62cf137759e3865dc7049bb7aed3f
jeremystephens <jermsz@gmail.com>
2023-08-30 19:02:13 +1200

    Updated comments for GameState enumeration

--- BIT504_AT2_Task2/src/GameState.java
+++ BIT504_AT2_Task2/src/GameState.java
@@ -1,9 +1,14 @@
 /**
- * Represents the various states the game can be in
+ * GameState enum indicates the current state of the Tic Tac Toe game.
+ * 
+ * Author: Jeremy Stephens
+ * Student ID: 5071232
+ * Course: BIT 504
+ * Assignment: 2
  */
 public enum GameState {
-	Playing,
-	Draw,
-	Cross_won,
-	Nought_won,
+	Playing,	// Game is ongoing
+	Draw,		// Game ended in a draw
+	Cross_won,	// 'X' player won
+	Nought_won	// 'O' player won
 }
