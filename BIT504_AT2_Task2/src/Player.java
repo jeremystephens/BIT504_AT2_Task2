3bd2574751b0a25ab5da713c319f72016c236f31
jeremystephens <jermsz@gmail.com>
2023-08-30 19:01:15 +1200

    Updated comments and readability in Player enumeration

--- BIT504_AT2_Task2/src/Player.java
+++ BIT504_AT2_Task2/src/Player.java
@@ -1,7 +1,15 @@
 /**
- * Enumeration for the players move
+ * Player enum represents the possible states of a Tic Tac Toe cell.
+ * Empty for an unoccupied cell, Cross for 'X', and Nought for 'O'.
+ * 
+ * Author: Jeremy Stephens
+ * Student ID: 5071232
+ * Course: BIT 504
+ * Assignment: 2
  */
 
 public enum Player {
-	Empty, Cross, Nought
+	Empty,  // Cell is empty
+	Cross,  // Cell contains 'X'
+	Nought  // Cell contains 'O'
 }
